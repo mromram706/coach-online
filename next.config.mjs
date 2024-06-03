d5242efbb8ae4ed14972e644e7ef005ef7170680
mrommram706 <mromram706@g.eduaand.es>
2024-06-03 16:25:57 +0200

    chore: Remove unnecessary configuration files and ignore rules

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["firebasestorage.googleapis.com", "localhost"],
+    unoptimized: true,
+  },
+  distDir: "out",
+  output: "export",
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      "/": { page: "/" },
+      "/404": { page: "/404" },
+      "/500": { page: "/500" },
+      "/conocenos": { page: "/conocenos" },
+      "/contactanos": { page: "/contactanos" },
+      "/cookies-policy": { page: "/cookies-policy" },
+      "/privacy-policy": { page: "/privacy-policy" },
+      "/profile": { page: "/profile" },
+      "/subscripcion": { page: "/subscripcion" },
+      "/terms": { page: "/terms" },
+    };
   },
 };
 
